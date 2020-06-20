4ac0b7f9527e8b9239e676062fcdd9846f789dc7
capeta0507 <nintendof1@gmail.com>
2020-06-20 11:45:31 +0800

    45

---
+++ summer_camping/js/index.js
@@ -0,0 +1,517 @@
+// 小孩
+const kidList = [
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/103",
+    "img": "img/card-img1.png",
+    "content_name1": "LEADERkids",
+    "content_name2": "2020 兒童運動旅遊",
+    "price": "NT$ 1800up",
+    "ticket": "再送GO Wild 300元購物金",
+    "nature": "戶外",
+    "place": "台北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/38",
+    "logo": "img/card-logo05.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/132",
+    "img": "img/card-img2.png",
+    "content_name1": "2020焦耳",
+    "content_name2": "小鐵人夏令營",
+    "price": "NT$ 6000",
+    "ticket": "再送Speedo 200元購物金",
+    "nature": "戶外",
+    "place": "台北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/83",
+    "logo": "img/card-logo14.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/103",
+    "img": "img/card-img3.png",
+    "content_name1": "洛德城堡鐵人探索",
+    "content_name2": "夏令營",
+    "price": "NT$ 6800up",
+    "ticket": "再送Speedo 200元購物金",
+    "nature": "戶外",
+    "place": "台北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/38",
+    "logo": "img/card-logo05.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/84",
+    "img": "img/card-img4.png",
+    "content_name1": "躍進MTB越野",
+    "content_name2": "夏令營",
+    "price": "NT$ 15000up",
+    "ticket": "再送GO Wild 300元購物金",
+    "nature": "戶外",
+    "place": "宜蘭縣",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/38",
+    "logo": "img/card-logo05.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/40",
+    "img": "img/card-img5.png",
+    "content_name1": "台東兒童夏令營",
+    "content_name2": "",
+    "price": "NT$ 16000up",
+    "ticket": "再送Speedo 200元購物金",
+    "nature": "戶外",
+    "place": "台東縣",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/55",
+    "logo": "img/card-logo06.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/24",
+    "img": "img/card-img6.png",
+    "content_name1": "澎湖兒童夏令營",
+    "content_name2": "",
+    "price": "NT$ 20000up",
+    "ticket": "再送Speedo 200元購物金",
+    "nature": "戶外",
+    "place": "澎湖縣",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/55",
+    "logo": "img/card-logo06.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/142",
+    "img": "img/card-img7.png",
+    "content_name1": "嘻波練家子夏令營",
+    "content_name2": "",
+    "price": "NT$ 7500",
+    "ticket": "再送GO Wild 300元購物金",
+    "nature": "戶外",
+    "place": "新北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/31",
+    "logo": "img/card-logo15.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/139",
+    "img": "img/card-img8.png",
+    "content_name1": "最新免插電",
+    "content_name2": "走繩體驗",
+    "price": "NT$ 300",
+    "ticket": "再送Speedo 200元購物金",
+    "nature": "戶外",
+    "place": "新北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/43",
+    "logo": "img/card-logo16.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/supplier/detail/65",
+    "img": "img/card-img9.png",
+    "content_name1": "2020攀岩夏令營",
+    "content_name2": "",
+    "price": "NT$ 1800up",
+    "ticket": "再送GO Wild 300元購物金",
+    "nature": "戶外",
+    "place": "新北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/65",
+    "logo": "img/card-logo04.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/supplier/detail/76",
+    "img": "img/card-img10.png",
+    "content_name1": "2020 spalding",
+    "content_name2": "籃球訓練營",
+    "price": "NT$ 3800up",
+    "ticket": "再送摩曼頓Nike500元購物金",
+    "nature": "籃球",
+    "place": "全台灣",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/76",
+    "logo": "img/card-logo17.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/supplier/detail/22",
+    "img": "img/card-img11.png",
+    "content_name1": "2020 夢想飛翔-",
+    "content_name2": "引爆暑假籃球訓練營",
+    "price": "NT$ 5080up",
+    "ticket": "再送摩曼頓Nike500元購物金",
+    "nature": "籃球",
+    "place": "全台灣",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/22",
+    "logo": "img/card-logo01.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/114",
+    "img": "img/card-img12.png",
+    "content_name1": "龍門獨木舟夏令營",
+    "content_name2": "",
+    "price": "NT$ 800",
+    "ticket": "再送GO Wild 300元購物金",
+    "nature": "水上",
+    "place": "新北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/13",
+    "logo": "img/card-logo03.png"
+  }
+]
+// 大人
+const grownList = [
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/117",
+    "img": "img/card-img13.png",
+    "content_name1": "假日玩耍團",
+    "content_name2": "龍門SUP體驗",
+    "price": "NT$ 1200",
+    "ticket": "再送GO Wild 300元購物金",
+    "nature": "水上",
+    "place": "新北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/13",
+    "logo": "img/card-logo03.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/56",
+    "img": "img/card-img14.png",
+    "content_name1": "獨木舟烏岩角",
+    "content_name2": "海蝕洞秘境沙灘",
+    "price": "NT$ 1800",
+    "ticket": "再送GO Wild 300元購物金",
+    "nature": "水上",
+    "place": "宜蘭縣",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/30",
+    "logo": "img/card-logo07.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/138",
+    "img": "img/card-img15.png",
+    "content_name1": "SUP立式划槳-",
+    "content_name2": "福隆雙溪河",
+    "price": "NT$ 1350",
+    "ticket": "再送GO Wild 300元購物金",
+    "nature": "水上",
+    "place": "新北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/30",
+    "logo": "img/card-logo18.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/140",
+    "img": "img/card-img16.png",
+    "content_name1": "戶外攀岩體驗",
+    "content_name2": "",
+    "price": "NT$ 600",
+    "ticket": "再送GO Wild 300元購物金",
+    "nature": "戶外",
+    "place": "新北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/43",
+    "logo": "img/card-logo19.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/118",
+    "img": "img/card-img17.png",
+    "content_name1": "海神宮溯溪",
+    "content_name2": "",
+    "price": "NT$ 2000",
+    "ticket": "再送GO Wild 300元購物金",
+    "nature": "戶外",
+    "place": "屏東縣",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/49",
+    "logo": "img/card-logo08.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/120",
+    "img": "img/card-img18.png",
+    "content_name1": "得子口溯溪",
+    "content_name2": "",
+    "price": "NT$ 1880",
+    "ticket": "再送GO Wild 300元購物金",
+    "nature": "戶外",
+    "place": "宜蘭縣",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/45",
+    "logo": "img/card-logo10.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/130",
+    "img": "img/card-img19.png",
+    "content_name1": "捎來溪探訪",
+    "content_name2": "龍谷大瀑布溯溪",
+    "price": "NT$ 2100",
+    "ticket": "再送GO Wild 300元購物金",
+    "nature": "戶外",
+    "place": "臺中市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/45",
+    "logo": "img/card-logo11.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/127",
+    "img": "img/card-img20.png",
+    "content_name1": "松鶴溪花式跳潭",
+    "content_name2": "悠遊溯溪",
+    "price": "NT$ 2300",
+    "ticket": "再送Speedo 200元購物金",
+    "nature": "戶外",
+    "place": "臺中市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/20",
+    "logo": "img/card-logo11.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/121",
+    "img": "img/card-img21.png",
+    "content_name1": "梅花溪溯溪",
+    "content_name2": "",
+    "price": "NT$ 2380",
+    "ticket": "再送GO Wild 300元購物金",
+    "nature": "戶外",
+    "place": "新竹縣",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/16",
+    "logo": "img/card-logo10.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/112",
+    "img": "img/card-img22.png",
+    "content_name1": "花式籃球球感",
+    "content_name2": "養成班",
+    "price": "NT$ 3500",
+    "ticket": "再送摩曼頓Nike500元購物金",
+    "nature": "籃球",
+    "place": "台北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/52",
+    "logo": "img/card-logo02.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/105",
+    "img": "img/card-img23.png",
+    "content_name1": "跑者專屬沙灘訓練",
+    "content_name2": "",
+    "price": "NT$ 540up",
+    "ticket": "再送摩曼頓Nike500元購物金",
+    "nature": "跑步",
+    "place": "宜蘭縣",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/36",
+    "logo": "img/card-logo12.png"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/event/detail/8",
+    "img": "img/card-img24.png",
+    "content_name1": "決戰萬米加速",
+    "content_name2": "訓練營",
+    "price": "NT$ 4680",
+    "ticket": "再送摩曼頓Nike500元購物金",
+    "nature": "跑步",
+    "place": "臺北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/17",
+    "logo": "img/card-logo13.png"
+  }
+]
+// 籃球
+const basketballList = [
+  {
+    "link": "https://www.o2gether.com.tw/product/detail/948",
+    "img": "img/card-img25.png",
+    "name1": "NIKE 青少年球衣",
+    "name2": "DRI-FIT吸濕排汗科技",
+    "old_price": "NT$2380",
+    "nature": "籃球",
+    "new_price": "NT$2142"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/product/detail/952",
+    "img": "img/card-img26.png",
+    "name1": "NIKE 青少年球褲",
+    "name2": "透氣網眼布料",
+    "old_price": "NT$1380",
+    "nature": "籃球",
+    "new_price": "NT$1242"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/product/detail/257",
+    "img": "img/card-img27.png",
+    "name1": "NIKE 中大童籃球鞋(紅藍)",
+    "name2": "限時71折",
+    "old_price": "NT$1800",
+    "nature": "籃球",
+    "new_price": "NT$1290"
+  },
+]
+// 水上
+const waterList = [
+  {
+    "link": "https://www.o2gether.com.tw/product/detail/999",
+    "img": "img/card-img28.png",
+    "name1": "BILLABONG衝浪褲",
+    "name2": "有後口袋設計",
+    "old_price": "NT$2190",
+    "nature": "水上",
+    "new_price": "NT$1970"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/product/detail/680",
+    "img": "img/card-img29.png",
+    "name1": "Feelfree坦克防水包30L",
+    "name2": "人體工學背負系統",
+    "old_price": "NT$2580",
+    "nature": "水上",
+    "new_price": "NT$1780"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/product/detail/18",
+    "img": "img/card-img30.png",
+    "name1": "ADIDAS中大童涼鞋(黑)",
+    "name2": "限時58折",
+    "old_price": "NT$1490",
+    "nature": "水上",
+    "new_price": "NT$860"
+  },
+]
+// 戶外
+const outdoorList = [
+  {
+    "link": "https://www.o2gether.com.tw/product/detail/715",
+    "img": "img/card-img31.png",
+    "name1": "The North Face 短袖上衣",
+    "name2": "限時76折",
+    "old_price": "NT$1480",
+    "nature": "戶外",
+    "new_price": "NT$1132"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/product/detail/744",
+    "img": "img/card-img32.png",
+    "name1": "The North Face 吸濕排汗短褲",
+    "name2": "限時76折",
+    "old_price": "NT$2380",
+    "nature": "戶外",
+    "new_price": "NT$1821"
+  },
+  {
+    "link": "https://www.o2gether.com.tw/product/detail/361",
+    "img": "img/card-img33.png",
+    "name1": "CONVERSE 側背包",
+    "name2": "限時79折",
+    "old_price": "NT$990",
+    "nature": "戶外",
+    "new_price": "NT$790"
+  },
+]
+
+let kidCard = ''
+let grownCard = ''
+let basketballCard = ''
+let waterCard = ''
+let outdoorCard = ''
+kidList.map(data => {
+  // console.log('data', data)
+  kidCard += `
+    <div class="card">
+      <a href="${data.link}" target="_blank" class="card-link">
+        <div class="card-img"><img src="${data.img}" alt="" class="img-rwd"></div>
+        <div class="card-content">
+          <h4 class="card-content-h4">${data.content_name1}</h4>
+          <h4 class="card-content-h4" style="height: 22px;">${data.content_name2}</h4>
+          <div class="card-content-style">
+            <img src="img/coin.png" alt="" class="middle">
+            <span class="middle mr18">${data.price}</span>    
+          </div>
+          <div class="card-content-style2">
+            <img src="img/ticket.png" alt="" class="middle">
+            <span class="middle mr18">${data.ticket}</span>    
+          </div>
+          <div class="card-content-bottom">
+            <div class="bg-o">${data.nature}</div>
+            <div class="bg-g">${data.place}</div>
+          </div>
+        </div>
+      </a>
+      <a href="${data.logo_link}" class="card-logo" target="_blank"><img src="${data.logo}" alt="" class="img-rwd"></a>
+    </div>
+  `
+})
+
+document.getElementById('kidContent').innerHTML = kidCard
+
+grownList.map(data => {
+  // console.log('data', data)
+  grownCard += `
+    <div class="card">
+      <a href="${data.link}" target="_blank" class="card-link">
+        <div class="card-img"><img src="${data.img}" alt="" class="img-rwd"></div>
+        <div class="card-content">
+          <h4 class="card-content-h4">${data.content_name1}</h4>
+          <h4 class="card-content-h4" style="height: 22px;">${data.content_name2}</h4>
+          <div class="card-content-style">
+            <img src="img/coin.png" alt="" class="middle">
+            <span class="middle mr18">${data.price}</span>    
+          </div>
+          <div class="card-content-style2">
+            <img src="img/ticket.png" alt="" class="middle">
+            <span class="middle mr18">${data.ticket}</span>    
+          </div>
+          <div class="card-content-bottom">
+            <div class="bg-o">${data.nature}</div>
+            <div class="bg-g">${data.place}</div>
+          </div>
+        </div>
+      </a>
+      <a href="${data.logo_link}" class="card-logo" target="_blank"><img src="${data.logo}" alt="" class="img-rwd"></a>
+    </div>
+  `;
+})
+document.getElementById('myGrown').innerHTML = grownCard
+
+basketballList.map(data => {
+  // console.log('data', data)
+  basketballCard += `
+    <div class="card">
+      <a href="${data.link}" target="_blank" class="card-link">
+        <div class="card-img2"><img src="${data.img}" alt="" class="img-rwd"></div>
+        <div class="black-content">
+          <h4 class="black-content-h4">${data.name1}</h4>
+          <h4>${data.name2}</h4>
+          <div class="black-content-style" style="text-align: right;">
+            ${data.old_price}
+          </div>
+          <div class="black-content-bottom">
+            <div class="bg-w">${data.nature}</div>
+            <h2>${data.new_price}</h2>
+          </div>
+        </div>
+      </a>
+    </div>
+  `;
+})
+document.getElementById('myBasketBall').innerHTML = basketballCard
+
+waterList.map(data => {
+  // console.log('data', data)
+  waterCard += `
+    <div class="card">
+      <a href="${data.link}" target="_blank" class="card-link">
+        <div class="card-img2"><img src="${data.img}" alt="" class="img-rwd"></div>
+        <div class="black-content">
+          <h4 class="black-content-h4">${data.name1}</h4>
+          <h4>${data.name2}</h4>
+          <div class="black-content-style" style="text-align: right;">
+            ${data.old_price}
+          </div>
+          <div class="black-content-bottom">
+            <div class="bg-w">${data.nature}</div>
+            <h2>${data.new_price}</h2>
+          </div>
+        </div>
+      </a>
+    </div>
+  `;
+})
+document.getElementById('myWater').innerHTML = waterCard
+
+waterList.map(data => {
+  // console.log('data', data)
+  outdoorCard += `
+    <div class="card">
+      <a href="${data.link}" target="_blank" class="card-link">
+        <div class="card-img2"><img src="${data.img}" alt="" class="img-rwd"></div>
+        <div class="black-content">
+          <h4 class="black-content-h4">${data.name1}</h4>
+          <h4>${data.name2}</h4>
+          <div class="black-content-style" style="text-align: right;">
+            ${data.old_price}
+          </div>
+          <div class="black-content-bottom">
+            <div class="bg-w">${data.nature}</div>
+            <h2>${data.new_price}</h2>
+          </div>
+        </div>
+      </a>
+    </div>
+  `;
+})
+document.getElementById('myOutdoor').innerHTML = outdoorCard
