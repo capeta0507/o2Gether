ae9ca0faab522b2a1c87d762cbaa3815636f4828
capeta0507 <nintendof1@gmail.com>
2020-07-10 19:54:48 +0800

    revitalize

---
+++ revitalize/js/index.js
@@ -0,0 +1,411 @@
+// 戶外
+const outdoorList = [
+  {
+    "link": "https://bit.ly/2AFQMVY",
+    "img": "img/card-img1.png",
+    "content_name1": "嘻波SO練家子夏令營",
+    "content_name2": "",
+    "price": "NT$ 7500",
+    "nature": "戶外",
+    "place": "新北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/31",
+    "logo": "img/card-logo01.png"
+  },
+  {
+    "link": "https://bit.ly/3iQD1F9",
+    "img": "img/card-img2.png",
+    "content_name1": "2020攀岩夏令營-",
+    "content_name2": "小飛鼠班",
+    "price": "NT$ 3800",
+    "nature": "戶外",
+    "place": "新北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/65",
+    "logo": "img/card-logo52.jpg"
+  },
+  {
+    "link": "https://bit.ly/2ZQy9Y1",
+    "img": "img/card-img3.png",
+    "content_name1": "LEADERkids",
+    "content_name2": "2020 兒童運動旅遊",
+    "price": "NT$ 1800~5980",
+    "nature": "戶外",
+    "place": "台北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/38",
+    "logo": "img/card-logo05.png"
+  },
+  {
+    "link": "https://bit.ly/3iNhGwe",
+    "img": "img/card-img4.png",
+    "content_name1": "合歡群峰百岳初體驗",
+    "content_name2": "(入門級)",
+    "price": "NT$ 7500",
+    "nature": "戶外",
+    "place": "南投縣",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/34",
+    "logo": "img/card-logo53.jpg"
+  },
+  {
+    "link": "https://bit.ly/3ffuUjj",
+    "img": "img/card-img5.png",
+    "content_name1": "躍進YI-LAN MTB",
+    "content_name2": "越野夏令營",
+    "price": "NT$ 16800",
+    "nature": "戶外",
+    "place": "宜蘭縣",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/38",
+    "logo": "img/card-logo05.png"
+  },
+  {
+    "link": "https://bit.ly/3eeG0Ux",
+    "img": "img/card-img6.png",
+    "content_name1": "戶外探索體驗 - ",
+    "content_name2": "攀樹 + 爬繩 + 滑索（三合一）",
+    "price": "NT$ 1800",
+    "nature": "戶外",
+    "place": "新北市",
+    "logo_link": "https://bit.ly/3eeG0Ux",
+    "logo": "img/card-logo54.jpg"
+  }
+]
+// 水上
+const waterList = [
+  {
+    "link": "https://bit.ly/3gH3m6I",
+    "img": "img/card-img13.png",
+    "content_name1": "史波奇暑期游泳密集班",
+    "content_name2": "",
+    "price": "NT$ 3000~4200",
+    "nature": "水上",
+    "place": "台北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/82",
+    "logo": "img/card-logo55.png"
+  },
+  {
+    "link": "https://bit.ly/3iMSjee",
+    "img": "img/card-img14.png",
+    "content_name1": "(玩水季獨家優惠)",
+    "content_name2": "獨木舟體驗+坦克防水包",
+    "price": "NT$ 1980",
+    "nature": "水上",
+    "place": "新北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/4",
+    "logo": "img/card-logo03.png"
+  },
+  {
+    "link": "https://bit.ly/3fcQuF3",
+    "img": "img/card-img15.png",
+    "content_name1": "假日玩耍團-SUP體驗",
+    "content_name2": "",
+    "price": "NT$ 1200",
+    "nature": "水上",
+    "place": "新北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/4",
+    "logo": "img/card-logo03.png"
+  },
+  {
+    "link": "https://bit.ly/38DVFM1",
+    "img": "img/card-img16.png",
+    "content_name1": "燿宇的游泳學校(海泳)",
+    "content_name2": "",
+    "price": "NT$ 1380",
+    "nature": "水上",
+    "place": "基隆市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/38",
+    "logo": "img/card-logo05.png"
+  },
+  {
+    "link": "https://bit.ly/3fcPTn5",
+    "img": "img/card-img17.png",
+    "content_name1": "獨家優惠【獨木舟經典行程—",
+    "content_name2": "划去拖回】烏岩角海蝕洞秘境沙灘",
+    "price": "NT$ 900~1800",
+    "nature": "水上",
+    "place": "宜蘭縣",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/30",
+    "logo": "img/card-logo07.png"
+  },
+  {
+    "link": "https://bit.ly/2W3fZRE",
+    "img": "img/card-img18.png",
+    "content_name1": "福隆黃金海",
+    "content_name2": "",
+    "price": "NT$ 1800",
+    "nature": "水上",
+    "place": "新北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/4",
+    "logo": "img/card-logo03.png"
+  }
+]
+// 健身
+const fitnessList = [
+  {
+    "link": "https://bit.ly/2BKkKZL",
+    "img": "img/card-img22.png",
+    "content_name1": "Get Fit 2020 ",
+    "content_name2": "健身不為誰 60天挑戰賽",
+    "price": "NT$ 6600",
+    "nature": "健身",
+    "place": "台北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/60",
+    "logo": "img/card-logo56.jpg"
+  },
+  {
+    "link": "https://bit.ly/2AM9OKy",
+    "img": "img/card-img23.png",
+    "content_name1": "科學健身、拳擊、",
+    "content_name2": "綜合格鬥一次滿足!!",
+    "price": "NT$ 5500",
+    "nature": "健身",
+    "place": "台中市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/84",
+    "logo": "img/card-logo57.jpg"
+  },
+  {
+    "link": "https://bit.ly/2Z9O70d",
+    "img": "img/card-img24.png",
+    "content_name1": "CrossFit夏日專案",
+    "content_name2": "",
+    "price": "NT$ 3300",
+    "nature": "健身",
+    "place": "台中市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/67",
+    "logo": "img/card-logo58.jpg"
+  }
+]
+// 籃球
+const basketballList = [
+  {
+    "link": "https://bit.ly/3ekoePT",
+    "img": "img/card-img19.png",
+    "content_name1": "2020 夢想飛翔·",
+    "content_name2": "引爆暑假籃球訓練營",
+    "price": "NT$ 5080~14640",
+    "nature": "籃球",
+    "place": "全台灣",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/22",
+    "logo": "img/card-logo51.png"
+  },
+  {
+    "link": "https://bit.ly/320A1A2",
+    "img": "img/card-img20.png",
+    "content_name1": "『夏令營』",
+    "content_name2": "花式籃球球感養成班-大人班",
+    "price": "NT$ 3500",
+    "nature": "籃球",
+    "place": "新北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/52",
+    "logo": "img/card-logo02.png"
+  },
+  {
+    "link": "https://bit.ly/2Cijih2",
+    "img": "img/card-img21.png",
+    "content_name1": "2020 Spalding",
+    "content_name2": "籃球訓練營",
+    "price": "NT$ 3800~4100",
+    "nature": "籃球",
+    "place": "全台灣",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/76",
+    "logo": "img/card-logo17.png"
+  }
+]
+// 跑步
+const runningList = [
+  {
+    "link": "https://bit.ly/2O7PDd9",
+    "img": "img/card-img25.png",
+    "content_name1": "『板橋』Akebono曙光 x",
+    "content_name2": "賽事備戰訓練營",
+    "price": "NT$ 4580~7980",
+    "nature": "跑步",
+    "place": "新北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/18",
+    "logo": "img/card-logo59.jpg"
+  },
+  {
+    "link": "https://bit.ly/3iDMn78",
+    "img": "img/card-img26.png",
+    "content_name1": "跑者專屬 沙灘訓練",
+    "content_name2": "",
+    "price": "NT$ 540~720",
+    "nature": "跑步",
+    "place": "宜蘭縣",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/36",
+    "logo": "img/card-logo12.png"
+  }
+]
+// 瑜珈
+const yogiList = [
+  {
+    "link": "https://bit.ly/38JT7vF",
+    "img": "img/card-img27.png",
+    "content_name1": "Yoga Kafe「基礎瑜珈」",
+    "content_name2": "專班 - 給初學者的瑜珈入門課程",
+    "price": "NT$ 2640",
+    "nature": "瑜珈",
+    "place": "新北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/24",
+    "logo": "img/card-logo60.png"
+  },
+  {
+    "link": "https://bit.ly/2ZbXDA2",
+    "img": "img/card-img28.png",
+    "content_name1": "美麗綻放 ‧ 女子力瑜珈",
+    "content_name2": "",
+    "price": "NT$ 800",
+    "nature": "瑜珈",
+    "place": "新北市",
+    "logo_link": "https://www.o2gether.com.tw/supplier/detail/24",
+    "logo": "img/card-logo60.png"
+  }
+]
+
+let waterCard = ''
+let outdoorCard = ''
+let fitnessCard = ''
+let basketballCard = ''
+let runningCard = ''
+let yogiCard = ''
+outdoorList.map(data => {
+  // console.log('data', data)
+  outdoorCard += `
+    <div class="card">
+      <a href="${data.link}" target="_blank" class="card-link">
+        <div class="card-img"><img src="${data.img}" alt="" class="img-rwd"></div>
+        <div class="card-content">
+          <h4 class="card-content-h4">${data.content_name1}</h4>
+          <h4 class="card-content-h4" style="height: 22px;">${data.content_name2}</h4>
+          <div class="card-content-style2">
+            <img src="img/coin.png" alt="" class="middle">
+            <span class="middle mr18">${data.price}</span>    
+          </div>
+          <div class="card-content-bottom">
+            <div class="bg-g">${data.place}</div>
+          </div>
+        </div>
+      </a>
+      <a href="${data.logo_link}" class="card-logo" target="_blank"><img src="${data.logo}" alt="" class="img-rwd"></a>
+    </div>
+  `;
+})
+document.getElementById('myOutdoor').innerHTML = outdoorCard
+
+waterList.map(data => {
+  // console.log('data', data)
+  waterCard += `
+    <div class="card">
+      <a href="${data.link}" target="_blank" class="card-link">
+        <div class="card-img"><img src="${data.img}" alt="" class="img-rwd"></div>
+        <div class="card-content">
+          <h4 class="card-content-h4">${data.content_name1}</h4>
+          <h4 class="card-content-h4" style="height: 22px;">${data.content_name2}</h4>
+          <div class="card-content-style2">
+            <img src="img/coin.png" alt="" class="middle">
+            <span class="middle mr18">${data.price}</span>    
+          </div>
+          <div class="card-content-bottom">
+            <div class="bg-g">${data.place}</div>
+          </div>
+        </div>
+      </a>
+      <a href="${data.logo_link}" class="card-logo" target="_blank"><img src="${data.logo}" alt="" class="img-rwd"></a>
+    </div>
+  `
+})
+
+document.getElementById('myWater').innerHTML = waterCard
+
+fitnessList.map(data => {
+  // console.log('data', data)
+  fitnessCard += `
+    <div class="card">
+      <a href="${data.link}" target="_blank" class="card-link">
+        <div class="card-img"><img src="${data.img}" alt="" class="img-rwd"></div>
+        <div class="card-content">
+          <h4 class="card-content-h4">${data.content_name1}</h4>
+          <h4 class="card-content-h4" style="height: 22px;">${data.content_name2}</h4>
+          <div class="card-content-style2">
+            <img src="img/coin.png" alt="" class="middle">
+            <span class="middle mr18">${data.price}</span>    
+          </div>
+          <div class="card-content-bottom">
+            <div class="bg-g">${data.place}</div>
+          </div>
+        </div>
+      </a>
+      <a href="${data.logo_link}" class="card-logo" target="_blank"><img src="${data.logo}" alt="" class="img-rwd"></a>
+    </div>
+  `;
+})
+document.getElementById('myFitness').innerHTML = fitnessCard
+
+basketballList.map(data => {
+  // console.log('data', data)
+  basketballCard += `
+    <div class="card">
+      <a href="${data.link}" target="_blank" class="card-link">
+        <div class="card-img"><img src="${data.img}" alt="" class="img-rwd"></div>
+        <div class="card-content">
+          <h4 class="card-content-h4">${data.content_name1}</h4>
+          <h4 class="card-content-h4" style="height: 22px;">${data.content_name2}</h4>
+          <div class="card-content-style2">
+            <img src="img/coin.png" alt="" class="middle">
+            <span class="middle mr18">${data.price}</span>    
+          </div>
+          <div class="card-content-bottom">
+            <div class="bg-g">${data.place}</div>
+          </div>
+        </div>
+      </a>
+      <a href="${data.logo_link}" class="card-logo" target="_blank"><img src="${data.logo}" alt="" class="img-rwd"></a>
+    </div>
+  `;
+})
+document.getElementById('myBasketball').innerHTML = basketballCard
+
+runningList.map(data => {
+  // console.log('data', data)
+  runningCard += `
+    <div class="card">
+      <a href="${data.link}" target="_blank" class="card-link">
+        <div class="card-img"><img src="${data.img}" alt="" class="img-rwd"></div>
+        <div class="card-content">
+          <h4 class="card-content-h4">${data.content_name1}</h4>
+          <h4 class="card-content-h4" style="height: 22px;">${data.content_name2}</h4>
+          <div class="card-content-style2">
+            <img src="img/coin.png" alt="" class="middle">
+            <span class="middle mr18">${data.price}</span>    
+          </div>
+          <div class="card-content-bottom">
+            <div class="bg-g">${data.place}</div>
+          </div>
+        </div>
+      </a>
+      <a href="${data.logo_link}" class="card-logo" target="_blank"><img src="${data.logo}" alt="" class="img-rwd"></a>
+    </div>
+  `;
+})
+document.getElementById('myRunning').innerHTML = runningCard
+
+yogiList.map(data => {
+  // console.log('data', data)
+  yogiCard += `
+    <div class="card">
+      <a href="${data.link}" target="_blank" class="card-link">
+        <div class="card-img"><img src="${data.img}" alt="" class="img-rwd"></div>
+        <div class="card-content">
+          <h4 class="card-content-h4">${data.content_name1}</h4>
+          <h4 class="card-content-h4" style="height: 22px;">${data.content_name2}</h4>
+          <div class="card-content-style2">
+            <img src="img/coin.png" alt="" class="middle">
+            <span class="middle mr18">${data.price}</span>    
+          </div>
+          <div class="card-content-bottom">
+            <div class="bg-g">${data.place}</div>
+          </div>
+        </div>
+      </a>
+      <a href="${data.logo_link}" class="card-logo" target="_blank"><img src="${data.logo}" alt="" class="img-rwd"></a>
+    </div>
+  `;
+})
+document.getElementById('myYoga').innerHTML = yogiCard
